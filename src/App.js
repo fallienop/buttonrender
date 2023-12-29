6481440f72e6bcc402334db5f5abf68b5405851a
fallienop <ulvi.babayev.200@gmail.com>
2023-12-29 12:40:22 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  
+  useEffect(()=>{
+    let toggleButton=document.getElementById('toggle');
+    toggleButton.style.background='red';
+  toggleButton.style.color='white';
+
+    let hoverButton=document.getElementById('hover');
+    hoverButton.style.background='red';
+    hoverButton.style.color='white';
+
+toggleButton.addEventListener('click',()=>{
+if(toggleButton.innerHTML=='Close'){
+  toggleButton.innerHTML='Open';
+  toggleButton.style.background='green';
+}
+else{
+  toggleButton.innerHTML='Close';
+  toggleButton.style.background='red';
+}
+
+});
+
+hoverButton.addEventListener('mouseover',(e)=>{
+  e.target.innerHTML='Hovered';
+  e.target.style.background='green';
+});
+hoverButton.addEventListener('mouseout',(e)=>{
+  e.target.innerHTML='Unhovered';
+  e.target.style.background='red';
+});
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+    
+    <button id='toggle'>Close</button>
+    <button id='hover'>Unhovered</button>
     </div>
   );
 }
